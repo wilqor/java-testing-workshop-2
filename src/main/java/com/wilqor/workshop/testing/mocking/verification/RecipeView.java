88deae2529e4ef6ba03a5153ff1ab8ff6e0ce7d3
wilqor <wilqor@gmail.com>
2018-01-24 11:52:18 +0100

    Update Javadoc of RecipeView

--- src/main/java/com/wilqor/workshop/testing/mocking/verification/RecipeView.java
+++ src/main/java/com/wilqor/workshop/testing/mocking/verification/RecipeView.java
@@ -2,16 +2,34 @@ package com.wilqor.workshop.testing.mocking.verification;
 
 /**
  * Interface imitating a UI view layer, responsible for
- * updating the UI.
+ * updating parts of the interface.
  *
  * @author wilqor
  */
 public interface RecipeView {
+    /**
+     * Clears all ingredient widgets in the panel
+     */
     void clearIngredientsPanel();
 
+    /**
+     * Adds widget for given ingredient to ingredients panel.
+     *
+     * @param ingredient ingredient to be added
+     */
     void addIngredientToPanel(Ingredient ingredient);
 
+    /**
+     * Configures display of gluten free label.
+     *
+     * @param visible flag describing display of gluten free label
+     */
     void setGlutenFreeLabelVisible(boolean visible);
 
+    /**
+     * Sets name to be displayed for given recipe.
+     *
+     * @param recipeName recipe name to be displayed
+     */
     void setRecipeNameText(String recipeName);
 }
