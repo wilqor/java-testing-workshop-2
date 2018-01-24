5e7ba5fa9ad13095bd56fdf43356ca4b60c58fb6
wilqor <wilqor@gmail.com>
2018-01-24 04:24:06 +0100

    Another improvement of mock configuration exercise description

--- src/test/java/com/wilqor/workshop/testing/mocking/configuration/exercises/ReturnChainingConfigurationExerciseTest.java
+++ src/test/java/com/wilqor/workshop/testing/mocking/configuration/exercises/ReturnChainingConfigurationExerciseTest.java
@@ -8,7 +8,7 @@ import com.wilqor.workshop.testing.mocking.configuration.TasksStream;
  * You will need to create a mock of {@link com.wilqor.workshop.testing.mocking.configuration.TasksStream}
  * to reuse across test cases and pass as argument to {@link com.wilqor.workshop.testing.mocking.configuration.TaskEstimator#esitmateEffort(TasksStream)}
  * as well as create the instance of system under test.
- * Think of possible test cases to test for:
+ * Think of possible test cases to check:
  * - empty stream of tasks
  * - stream with estimate {@link com.wilqor.workshop.testing.mocking.configuration.Effort#TOO_LOW}
  * - stream with estimate {@link com.wilqor.workshop.testing.mocking.configuration.Effort#ALL_RIGHT}
